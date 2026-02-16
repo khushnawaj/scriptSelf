34317427429076775770885173858e5a8df8941b
khushnawaj <nawajkhush86@gmail.com>
2026-02-16 14:57:56 +0530

    fix: Increase Multer field limits to 50MB to allow large text content

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -3,7 +3,10 @@ const { storage } = require('../config/cloudinary');
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 10000000 }, // 10MB limit
+    limits: {
+        fileSize: 50 * 1024 * 1024, // 50MB file
+        fieldSize: 50 * 1024 * 1024 // 50MB text field (Note content)
+    },
 });
 
 module.exports = upload;
